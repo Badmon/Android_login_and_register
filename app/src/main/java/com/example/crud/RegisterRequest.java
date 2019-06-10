b18eb40cabf458652cf2152bcd2d5538ed9ce007
Badmon <jleonh@autonoma.edu.pe>
2019-06-10 03:45:12 -0500

    Commit

---
+++ app/src/main/java/com/example/crud/RegisterRequest.java
@@ -0,0 +1,30 @@
+package com.example.crud;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+
+    private static final String REGISTER_REQUEST_URL="http://192.168.1.10:8080/Registro.php";
+
+    private Map<String,String> params;
+    public RegisterRequest(String name, String username, int age, String
+            password,Response.Listener<String> Listener){
+        super(Method.POST,REGISTER_REQUEST_URL, Listener,null);
+
+        params=new HashMap<>();
+        params.put("name",name);
+        params.put("username",username);
+        params.put("age",age+"");
+        params.put("password",password);
+
+    }
+
+    @Override
+    public Map<String, String> getParams() {
+        return params;
+    }
+}
--- app/src/main/java/com/example/crud/Registro.java
+++ app/src/main/java/com/example/crud/Registro.java
@@ -1,14 +1,78 @@
 package com.example.crud;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Registro extends AppCompatActivity {
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.ResponseCache;
+
+public class Registro extends AppCompatActivity implements View.OnClickListener {
+
+    EditText enombre,eusuario,epassword,eage;
+    Button btn_registrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        enombre = findViewById(R.id.EditT_nombre);
+        eusuario = findViewById(R.id.EditT_usu);
+        epassword = findViewById(R.id.EditT_password);
+        eage = findViewById(R.id.EdiT_edad);
+
+        btn_registrar = findViewById(R.id.btn_registrar);
+
+        btn_registrar.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        final String name=enombre.getText().toString();
+        final String username=eusuario.getText().toString();
+        final int age=Integer.parseInt(eage.getText().toString());
+        final String password=epassword.getText().toString();
+
+        Response.Listener<String> respoListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean("success");
+
+                    if (success){
+                            Intent intent = new Intent(Registro.this,Login.class);
+                            Registro.this.startActivity(intent);
+                    }else{
+                        AlertDialog.Builder builder = new AlertDialog.Builder(Registro.this);
+                        builder.setMessage("Error registro")
+                                .setNegativeButton("Retry",null)
+                                .create().show();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        RegisterRequest registerRequest = new RegisterRequest(name,username,age,password,respoListener);
+            RequestQueue queue = Volley.newRequestQueue(Registro.this);
+            queue.add(registerRequest);
+
     }
 }
