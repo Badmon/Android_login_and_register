67f04265de1433d787dd0b269db9bf130f295436
Badmon <jleonh@autonoma.edu.pe>
2019-06-09 20:53:25 -0500

    Commit

--- app/src/main/java/com/example/crud/MainActivity.java
+++ app/src/main/java/com/example/crud/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.crud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_registrar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_registrar = findViewById(R.id.tv_registrar);
+
+        tv_registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intentReg = new Intent(MainActivity.this,Registro.class);
+                MainActivity.this.startActivity(intentReg);
+            }
+        });
     }
 }
